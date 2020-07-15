fe188bd6f43c631abfe9414c562b0b6702be3c14
carrliitos <salazar.ten80@gmail.com>
2020-07-15 10:44:15 -0500

    Sync example added

---
+++ Async/app.js
@@ -0,0 +1,16 @@
+// sync code example
+
+function otherFunct(){
+	console.log("Other function");
+	console.log("Another function");
+}
+
+console.log("Start");
+
+otherFunct();
+
+setTimeout(() => {
+	console.log("Inside timeout after 5 seconds");
+}, 5000);
+
+console.log("End");
