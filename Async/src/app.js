01a218d42862dc3a56ee7618d7d7581bf967253a
carrliitos <salazar.ten80@gmail.com>
2020-07-15 10:53:58 -0500

    added getUserVideos function

--- Async/src/app.js
+++ Async/src/app.js
@@ -7,10 +7,30 @@ function otherFunct(){
 
 console.log("Start");
 
-otherFunct();
+// setTimeout(() => {
+// 	console.log("Inside timeout after 5 seconds");
+// }, 5000);
 
-setTimeout(() => {
-	console.log("Inside timeout after 5 seconds");
-}, 5000);
+function loginUser(email, password, callback) {
+	setTimeout(() => {
+		console.log("Now we have the data");
+		callback( {userEmail : email});
+	}, 3000);
+}
+
+function getUserVideos(email, callback) {
+	setTimeout(() => {
+		callback( ["video1", "video2", "video3"]);
+	}, 2000);
+}
+
+const user = loginUser("salazar@nicemail.com", 123456, (user) => {
+	console.log(user);
+	getUserVideos(user.userEmail, (videos) => {
+		console.log(videos);
+	})
+});
+
+// console.log(user);
 
 console.log("End");	
